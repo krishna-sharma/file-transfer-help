6532d19ce2da2429093d8217a70f6a4310875abe
Krishna Sharma <krishnasharma1495@gmail.com>
2022-01-26 12:22:18 +0530

    Ability to send file meta data to server

--- src/index.js
+++ src/index.js
@@ -18,6 +18,17 @@ const wss = new Server({ server });
 wss.on("connection", (ws) => {
   console.log("Client connected");
   ws.on("close", () => console.log("Client disconnected"));
+
+  ws.on("message", (data, isBinary) => {
+    if (isBinary) {
+      console.log("Received binary data of length", Buffer.byteLength(data, "utf-8"));
+    } else {
+      console.log("Received non-binary data ", data.toString("utf-8"));
+    }
+  });
+
+  const currentTime = new Date().toISOString().replace("T", " ").slice(0, -5);
+  ws.send(currentTime);
 });
 
 setInterval(() => {
