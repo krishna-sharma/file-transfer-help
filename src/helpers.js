7e1e9983b31f32399b68788bbd142def8c944ea6
Krishna Sharma <krishnasharma1495@gmail.com>
2022-01-26 19:15:29 +0530

    another round of code refactor

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,23 @@
-exports.addFileToList = (files, newFileMeta, fileId, clientId) => {
+const clients = {};
+const files = {};
+let lastUsedClientId = 0;
+let lastUsedFileId = 0;
+
+exports.addNewClient = (clientId, ws) => {
+  clients[clientId] = {
+    clientId,
+    webSocket: ws,
+    files: [],
+  };
+};
+
+exports.deleteClient = (clientId) => {
+  // delete file entries from clientId in files
+  // delete file entries from clientId in other clients
+  delete clients[clientId];
+};
+
+exports.addFileToList = (newFileMeta, fileId, clientId) => {
   files[fileId] = {
     clientId,
     fileId,
@@ -6,6 +25,21 @@ exports.addFileToList = (files, newFileMeta, fileId, clientId) => {
   };
 };
 
-exports.addFileToClient = (clients, clientId, fileId) => {
+exports.addFileToClient = (clientId, fileId) => {
   clients[clientId].files.push(fileId);
+};
+
+exports.doOnAllClients = (cb) => Object.values(clients).map(cb);
+
+exports.filesListForClient = (clientId) =>
+  JSON.stringify(Object.values(files).filter((file) => file.clientId !== clientId));
+
+exports.getNextClientid = () => {
+  lastUsedClientId += 1;
+  return `client-${lastUsedClientId}`;
+};
+
+exports.getNextFileid = () => {
+  lastUsedFileId += 1;
+  return `client-${lastUsedFileId}`;
 };
--- src/webSocketServer.js
+++ src/webSocketServer.js
@@ -1,40 +1,35 @@
 const { Server } = require("ws");
-const { addFileToList, addFileToClient } = require("./helpers");
-
-const clients = {};
-let lastUsedClientId = 0;
-const files = {};
-let lastUsedFileId = 0;
+const {
+  getNextClientid,
+  getNextFileid,
+  addNewClient,
+  deleteClient,
+  addFileToList,
+  addFileToClient,
+  filesListForClient,
+  doOnAllClients,
+} = require("./helpers");
 
 const connection = (clientId, ws) => {
-  clients[clientId] = {
-    clientId,
-    webSocket: ws,
-    files: [],
-  };
-  const filesList = JSON.stringify(Object.values(files));
-  ws.send(filesList, { binary: false });
+  addNewClient(clientId, ws);
+  ws.send(filesListForClient(clientId), { binary: false });
 };
 
 const close = (clientId) => {
-  // delete file entries from clientId in files
-  // delete file entries from clientId in other clients
-  delete clients[clientId];
+  deleteClient(clientId);
 };
 
 const message = (clientId, data, isBinary) => {
   if (isBinary) {
     // console.log("Received binary data of length", Buffer.byteLength(data, "utf-8"));
   } else {
-    const currentFileId = `client-${lastUsedFileId}`;
+    const fileId = getNextFileid();
     // delete old file entries from clientId in files
     // delete old file entries from clientId in other clients
-    addFileToList(files, data.toString("utf-8"), currentFileId, clientId);
-    addFileToClient(clients, clientId, currentFileId);
-    lastUsedFileId += 1;
-    Object.values(clients).forEach((client) => {
-      const filesList = JSON.stringify(Object.values(files).filter((file) => file.clientId !== client.clientId));
-      client.webSocket.send(filesList, { binary: false });
+    addFileToList(data.toString("utf-8"), fileId, clientId);
+    addFileToClient(clientId, fileId);
+    doOnAllClients((client) => {
+      client.webSocket.send(filesListForClient(client.clientId), { binary: false });
     });
   }
 };
@@ -42,11 +37,9 @@ const message = (clientId, data, isBinary) => {
 exports.startWebSocketServer = (httpServer) => {
   const wss = new Server({ server: httpServer });
   wss.on("connection", (ws) => {
-    const currentClientId = `client-${lastUsedClientId}`;
-    lastUsedClientId += 1;
-
-    connection(currentClientId, ws);
-    ws.on("close", () => close(currentClientId));
-    ws.on("message", (data, isBinary) => message(currentClientId, data, isBinary));
+    const clientId = getNextClientid();
+    connection(clientId, ws);
+    ws.on("close", () => close(clientId));
+    ws.on("message", (data, isBinary) => message(clientId, data, isBinary));
   });
 };
