4b0884cd070593ea917daaff7f9366c260f88a3d
Krishna Sharma <krishnasharma1495@gmail.com>
2022-04-14 08:49:13 +0530

    Add a basic webrtc file

--- src/httpServer.js
+++ src/httpServer.js
@@ -6,11 +6,19 @@ const PORT = process.env.PORT || 8000;
 
 exports.startHTTPServer = () => {
   const indexContents = fs.readFileSync(path.join(__dirname, "index.html"), "utf8");
+  const webrtcContents = fs.readFileSync(path.join(__dirname, "webrtc.html"), "utf8");
 
   const app = express();
+
+  app.get("/webrtc", (req, res) => {
+    res.type(".html");
+    res.status(200).send(webrtcContents);
+  });
+
   app.get("/", (req, res) => {
     res.type(".html");
     res.status(200).send(indexContents);
   });
+
   return app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 };
