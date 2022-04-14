f851731a5a91361b74f6b7933e6b76e0bb22a70b
Krishna Sharma <krishnasharma1495@gmail.com>
2022-04-14 12:46:02 +0530

    Ability to add peers when pag is loaded

--- src/webrtc.js
+++ src/webrtc.js
@@ -3,10 +3,22 @@ const fs = require("fs");
 const path = require("path");
 const router = express.Router();
 
+const peers = [];
+
 const webrtcContents = fs.readFileSync(path.join(__dirname, "webrtc.html"), "utf8");
 
-router.get("/webrtc/one", function (req, res) {
-  res.status(200).json({ one: 1 });
+router.post("/webrtc/internals", function (req, res) {
+  const internals = {};
+  if (req.body.peers) internals.peers = peers;
+  res.status(200).json(internals);
+});
+
+router.post("/webrtc/peer/add", function (req, res) {
+  peers.push({
+    id: peers.length,
+    ...req.body,
+  });
+  res.status(200).json({ peers: peers });
 });
 
 router.get("/webrtc", (req, res) => {
